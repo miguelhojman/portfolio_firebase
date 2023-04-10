1ee004c44a38a07f19a71f1cf58c61e910d95084
Miguel Hojman <miguelhojman@gmail.com>
2023-04-10 15:40:24 -0300

    punto de control previo al login

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -9,7 +9,7 @@ import { PersonaService } from 'src/app/servicios/persona.service';
 })
 export class FooterComponent {
   persona:Persona=new Persona('','','','','','','','','','','','','');
-  //persona:any;
+  isLogged=true;
   constructor(public personaService:PersonaService){}
 
   ngOnInit(): void {
