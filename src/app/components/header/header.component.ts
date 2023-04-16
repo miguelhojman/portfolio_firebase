afdd2b6515c04bff6d259b3533c1bab4581503d7
Miguel Hojman <miguelhojman@gmail.com>
2023-04-16 09:56:05 -0300

    ya funciona edicion en el header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Persona } from 'src/app/modelos/persona';
 import { ModoeditService } from 'src/app/servicios/modoedit.service';
 import { PersonaService } from 'src/app/servicios/persona.service';
+import { FootereditService } from 'src/app/serviciosedicion/footeredit.service';
 
 @Component({
   selector: 'app-header',
@@ -12,8 +13,16 @@ export class HeaderComponent {
   
   persona:Persona=new Persona('','','','','','','','','','','','','');
   isLogged:boolean=false;
+  nuevoMail:String=this.persona.mail;
+  nuevoLinkedin:String=this.persona.linkedin;
+  nuevoGithub:String=this.persona.github;
+  nuevoFacebook:String=this.persona.facebook;
+  nuevoTwitter:String=this.persona.twitter;
+  
 
-  constructor(public personaService:PersonaService,public modoedit:ModoeditService){}
+  constructor(public personaService:PersonaService,
+              public modoedit:ModoeditService,
+              public footerEdit:FootereditService){}
 
   ngOnInit(): void {
       this.personaService.traerPersona().subscribe(data=>{
@@ -29,6 +38,16 @@ export class HeaderComponent {
     window.location.reload();
   }
 
+  actualizar():void{
+    this.persona.mail=this.nuevoMail;
+    this.persona.linkedin=this.nuevoLinkedin;
+    this.persona.github=this.nuevoGithub;
+    this.persona.facebook=this.nuevoFacebook;
+    this.persona.twitter=this.nuevoTwitter;
+    this.footerEdit.editar(this.persona).subscribe(data=>{
+      this.persona=data;
+    });
+  }
   
 
 
