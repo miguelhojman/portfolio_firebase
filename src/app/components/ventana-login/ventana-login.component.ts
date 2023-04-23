ca1e884c30dd2a529cf20a20f640c56c057f79bf
Miguel Hojman <miguelhojman@gmail.com>
2023-04-23 17:32:00 -0300

    voy a hacer el modal de error al loguearse

--- src/app/components/ventana-login/ventana-login.component.ts
+++ src/app/components/ventana-login/ventana-login.component.ts
@@ -6,30 +6,26 @@ import { ModoeditService } from 'src/app/servicios/modoedit.service';
 @Component({
   selector: 'app-ventana-login',
   templateUrl: './ventana-login.component.html',
-  styleUrls: ['./ventana-login.component.css']
+  styleUrls: ['./ventana-login.component.css'],
 })
 export class VentanaLoginComponent {
+  usuario1: String = '';
+  password1: String = '';
+  seHaLogueado: boolean = false;
+  user: UserLogin = new UserLogin('', '');
+  constructor(
+    private loginService: LoginService,
+    public modoedit: ModoeditService
+  ) {}
 
-  usuario1:String='';
-  password1:String='';
-
-seHaLogueado:boolean=false;
-user:UserLogin=new UserLogin('','');
-constructor(private loginService:LoginService,public modoedit:ModoeditService){}
-
-recibirLogin():void{
-  this.user=new UserLogin(this.usuario1,this.password1);
-
-    this.loginService.enviarLogin(this.user).subscribe(data=>{
-    this.seHaLogueado=data;
-    console.log("Se ha logueado vale: "+this.seHaLogueado);
-    console.log(this.usuario1);
-    console.log(this.password1);
-    this.modoedit.disparador.emit(this.seHaLogueado);
-    if(this.seHaLogueado==false)
-      alert("CREDENCIALES NO VALIDAS");              
-    })
+  recibirLogin(): void {
+    this.user = new UserLogin(this.usuario1, this.password1);
+    this.loginService.enviarLogin(this.user).subscribe((data) => {
+      this.seHaLogueado = data;
+      this.modoedit.disparador.emit(this.seHaLogueado);
+      if (this.seHaLogueado == false) {
+        alert('CREDENCIALES NO VALIDAS');
+      }
+    });
   }
 }
-
-  
